02708a1296f548e0f98005136ec5bb8697adf423
Gabru678 <Paulbath7@gmail.com>
2016-02-01 17:44:10 -0500

    First commit Main and Collections not working

---
+++ CIS 225 Assignment 1/CIS 225 Assignment 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_225_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our wacky and wild wine list! \n");
+
+            int UserIn = UserInterface.GetUserInput();
+
+            // Assuming GetUserInput returns an integer of input
+            // Well it still wants an integer?
+            // Kind of Lost at this stage
+            if (UserIn = 1)
+            {
+
+            } 
+            else if ()
+        }
+    }
+}
---
+++ CIS 225 Assignment 1/CIS 225 Assignment 1/WineItemCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_225_Assignment_1
+{
+    class WineItemCollection
+    {
+        // Create an array of WineItems here
+        WineItem[] WineArray = new WineItem[0];
+        
+        //There may be a loop to print them out?
+        // but couldnt we call the PrintAllValues from the WineItem class?
+
+    }
+}
